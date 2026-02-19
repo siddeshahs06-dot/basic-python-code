91f6cca3630c6364c69680d280c927c740c49e88
siddesh <siddeshahs06@gmail.com>
2026-02-19 10:08:03 +0530

    2nd commit

--- dict1.py
+++ dict1.py
@@ -1,4 +1,4 @@
-dict={}
+dict3={}
 for i in range(0,2):
     student=input("enter a student ID")
     dict1={}
@@ -6,5 +6,5 @@ for i in range(0,2):
         key=input("enter a key")
         value=input("enter a value")
         dict1[key]=value
-    dict[student]=dict1    
-print(dict)    
+    dict3[student]=dict1    
+print(dict3)    
